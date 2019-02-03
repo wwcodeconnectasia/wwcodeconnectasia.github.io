66025762e4ba2aefd07eafcc6a108f037c0b6f9c
wendko <wendko.88@gmail.com>
2019-02-04 02:01:33 +0800

    scrolling animation

--- code/src/app/app.component.ts
+++ code/src/app/app.component.ts
@@ -4,14 +4,13 @@ import organizers from '../assets/data/organizers';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   speakerProfileLink = 'http://goo.gl/forms/YrRPwmaefr3Kf43q2';
   callForProposalLink = 'http://goo.gl/forms/yHvVXsgAffutFM5x1';
   wwcodeLogoSrc = '../assets/images/wwc_logo.jpeg';
   speakerSrc = '../assets/images/speaker.png';
-
   countries = organizers;
 
   redirect(destination: string) {
