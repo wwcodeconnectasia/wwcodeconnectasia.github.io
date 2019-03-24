efe15d198a6a0a8819e715e470c66c8b006a8c41
wendko <wendko.88@gmail.com>
2019-03-25 02:11:03 +0800

    hashtag and email changes

--- code/src/app/app.component.ts
+++ code/src/app/app.component.ts
@@ -9,9 +9,12 @@ import organizers from '../assets/data/organizers';
 export class AppComponent {
   speakerProfileLink = 'http://goo.gl/forms/YrRPwmaefr3Kf43q2';
   callForProposalLink = 'http://goo.gl/forms/yHvVXsgAffutFM5x1';
-  wwcodeLogoSrc = '../assets/images/wwc_logo.jpeg';
-  speakerSrc = '../assets/images/speaker.png';
+  wwcodeLogoSrc = 'assets/images/wwc_logo.jpeg';
+  speakerSrc = 'assets/images/speaker.png';
   countries = organizers;
+  email = 'connectasia@womenwhocode.com';
+
+  hashtags: string[] = ['#CONNECTAsia', '#WWCode'];
 
   redirect(destination: string) {
     let link;
